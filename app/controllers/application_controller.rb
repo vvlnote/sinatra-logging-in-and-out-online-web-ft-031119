a06feecec583ea3a89d80efc35cc8462b255b9a1
Joy Lin <vvlnote@hotmail.com>
2019-05-06 20:59:55 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,8 +23,9 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/account' do
-    @user = User.find(session[:user_id])
-    erb :account
+    if Helper.is_logged_in(session)
+      @user = User.find(session[:user_id])
+      erb :account
   end
 
   get '/logout' do
