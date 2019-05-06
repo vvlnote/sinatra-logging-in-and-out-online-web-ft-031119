3c028f283e22bdb5e3a4ec2a27b08fbbbaf0af60
Joy Lin <vvlnote@hotmail.com>
2019-05-06 21:24:51 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -6,8 +6,8 @@ class Helpers
   
   def self.is_logged_in?(session)
     #session.has_key?('user_id')? ture : false
-    binding.pry
-    if session.has_key?('user_id'.to_s)
+    #binding.pry
+    if session.has_key?(:user_id)
       true
     else
       false
