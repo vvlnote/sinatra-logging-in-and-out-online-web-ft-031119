0eb6c82b22652919fe0131992679a4e7e6fcfc37
Joy Lin <vvlnote@hotmail.com>
2019-05-06 21:04:43 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,7 +24,7 @@ class ApplicationController < Sinatra::Base
 
   get '/account' do
     if Helpers.is_logged_in?(session)
-      @user = Helper.current_user(session)
+      @user = Helpers.current_user(session)
       erb :account
     else
       redirect to ('/')
