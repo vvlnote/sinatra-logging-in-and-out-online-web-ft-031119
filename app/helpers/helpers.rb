9c2e691df89a3281082b34fc02fe153a4964b2c8
Joy Lin <vvlnote@hotmail.com>
2019-05-06 21:31:41 +0000

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,16 @@
 class Helpers
+  
+  def self.current_user(session)
+    @user = User.find(session[:user_id])
+  end
+  
+  def self.is_logged_in?(session)
+    #session.has_key?('user_id')? ture : false
+    #binding.pry
+    if session.has_key?(:user_id)
+      true
+    else
+      false
+    end
+  end
 end
